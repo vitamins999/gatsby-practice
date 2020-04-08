c2a8b3c7434010634b07889d5a4d95958c3ac2b1
Jools Barnett <vitamins999@gmail.com>
2020-04-08 17:24:48 +0100

    Removed contact info

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home" />
             <h1>Hello.</h1>
             <h2>I'm Jools.  A half man half kitten.  Kudos!</h2>
-            <p>Need a cat? <Link to="/contact">Contact Me</Link></p>
         </Layout>
     )
 }
